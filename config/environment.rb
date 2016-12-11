df7d6627c65ece8c1832095d428f2c4041817f9f
Laurent <lmilne360@gmail.com>
2016-12-10 23:02:04 -0500

    Updated configurations

---
+++ config/environment.rb
@@ -0,0 +1,10 @@
+require 'bundler/setup'
+Bundler.require
+
+require 'require_all'
+require_all 'app'
+
+ActiveRecord::Base.establish_connection(
+  :adapter => "sqlite3",
+  :database => "db/#{ENV['SINATRA_ENV']}.sqlite"
+)
