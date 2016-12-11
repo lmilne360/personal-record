5fd76af634d63e3c11c235719c42bdfcfbb5f229
Laurent <lmilne360@gmail.com>
2016-12-11 00:16:43 -0500

    Added exercise generation

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,3 +1,12 @@
 class ExercisesController < ApplicationController
 
+
+	get '/exercise/new' do 
+		erb :'exercise/new_exercise'
+	end
+
+	post '/exercise' do 
+		ex = Exercise.new(name: params[:name])
+		binding.pry
+	end
 end
