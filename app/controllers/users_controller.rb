abc6c8b6c4d234a4f3dfa41124787a60f5345e38
Laurent <lmilne360@gmail.com>
2016-12-11 00:05:49 -0500

    Added sidn_up route

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,3 +1,14 @@
 class UsersController < ApplicationController
 
+	get '/sign_up' do 
+		erb :'user/new_user'
+	end
+
+	post '/users' do
+		user = User.create(username: params[:username], password: params[:password])
+		session[:user_id] = user.id
+		redirect '/'
+	end
+
+
 end
