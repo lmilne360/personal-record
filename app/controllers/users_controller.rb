d03a7f77270debf69a83e6d90a34226abc8e099f
Laurent <lmilne360@gmail.com>
2016-12-11 18:31:26 -0500

    Updated Routing

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,25 +1,30 @@
 class UsersController < ApplicationController
 
 	get '/signup' do 
-		erb :'users/new_user'
+		erb :'users/new_user' unless logged_in?
+		redirect "/users/#{current_user.id}"
 	end
 
 	post '/users' do
 		user = User.create(username: params[:username], password: params[:password])
 		session[:user_id] = user.id
-		redirect '/'
+		redirect to "/users/#{current_user.id}"
 	end
 
 
 	get '/login' do
-		erb :'users/login'
+		if logged_in?
+			redirect "/users/#{current_user.id}"
+		else
+			erb :'users/login'
+		end
 	end
 
 	post '/login' do 
 		user = User.find_by(username: params[:username])
 		if user && user.authenticate(params[:password])
 			session[:user_id] = user.id
-			erb :'users/show'
+			redirect to "/users/#{current_user.id}"
 		else
 			redirect '/signup'
 		end
@@ -27,8 +32,21 @@ class UsersController < ApplicationController
 	end
 
 	get '/logout' do 
-		session.clear
-		redirect '/'
+		if logged_in?
+			session.clear 
+			redirect '/'
+		else
+			redirect '/'
+		end
+		
+	end
+
+	get	'/users/:id' do
+	 if logged_in? 
+	 	erb :'users/show'
+	 else
+	 	redirect '/login'
+	 end
 	end
 
 
