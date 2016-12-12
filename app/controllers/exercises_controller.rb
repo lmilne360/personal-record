9c7dcc7533616587fb591cd6286819b5b75dc482
Laurent <lmilne360@gmail.com>
2016-12-11 20:33:19 -0500

    Updated exercise adding/editing

--- app/controllers/exercises_controller.rb
+++ app/controllers/exercises_controller.rb
@@ -1,12 +1,38 @@
 class ExercisesController < ApplicationController
 
 
-	get '/exercise/new' do 
-		erb :'exercise/new_exercise'
+	get '/exercise/new' do
+		if logged_in? 
+			erb :'exercises/new_exercise'
+		else 
+			redirect '/login'
+		end
 	end
 
 	post '/exercise' do 
 		ex = Exercise.new(name: params[:name])
-		binding.pry
+		ex.user_id = current_user.id 
+		ex.save
+		redirect to "/users/#{current_user.id}"
 	end
+
+	get '/exercise/:id' do
+		if logged_in?
+			@ex = Exercise.find_by_id(params[:id])
+		erb :'exercises/update_exercise'
+		else 
+		redirect '/login'
+		end
+	end
+
+	patch '/exercise/:id' do
+		ex = Exercise.find_by_id(params[:id])
+		binding.pry
+		ex.update(reps: params[:reps]) unless params[:reps].empty?
+		ex.update(weight: params[:weight]) unless params[:weight].empty?
+		redirect to "/users/#{current_user.id}"
+	end
+
+
+
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,8 +1,12 @@
 class UsersController < ApplicationController
 
-	get '/signup' do 
-		erb :'users/new_user' unless logged_in?
-		redirect "/users/#{current_user.id}"
+	get '/signup' do
+		if logged_in?
+			redirect "/users/#{current_user.id}"
+		else
+			erb :'users/new_user'
+		end
+		
 	end
 
 	post '/users' do
