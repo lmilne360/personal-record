2320c8bad9d92c5beefdc700332d7b219b122c60
Laurent <lmilne360@gmail.com>
2016-12-12 02:04:21 -0500

    Change exercise.user_id to Integer

--- db/schema.rb
+++ db/schema.rb
@@ -10,11 +10,11 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20161211234605) do
+ActiveRecord::Schema.define(version: 20161212070045) do
 
   create_table "exercises", force: :cascade do |t|
     t.string  "name"
-    t.string  "user_id"
+    t.integer "user_id"
     t.integer "reps"
     t.integer "weight"
   end
